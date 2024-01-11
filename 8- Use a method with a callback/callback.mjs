dd92da285bdc3ea9e3e24c66394628595f7a4004
Marta <marta.ferraris25@gmail.com>
2024-01-11 23:43:33 +0100

    exercise 8

--- 8- Use a method with a callback/callback.mjs
+++ 8- Use a method with a callback/callback.mjs
@@ -1,13 +1,10 @@
-import * as fs from "node:fs";
+import { writeFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
 
-fs.writeFile("file.txt", {encoding: "utf-8"} , function (error, data) {
-    if(error) {
-        console.error(error);
-        return;
-    }
-
-    console.log(data)
+const data = new Uint8Array(Buffer.from('Hello Node.js'));
+writeFile('file.txt', data, (err) => {
+    if (err) throw err;
+    console.log('The file is writed.');
 });
-
 
 /* write(fd, Buffer.from(data, options.encoding), callback);  */
