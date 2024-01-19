837f181f4d719a72781e84950b4984ec8d07dee9
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2024-01-19 12:10:16 +0100

    exercise 12

--- 12- Set up a simple Express App/server.ts
+++ 12- Set up a simple Express App/server.ts
@@ -1,3 +1,8 @@
+import express from 'express';
+import 'express-async-errors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
 type Planet = {
     id: number,
     name: string,
@@ -15,3 +20,26 @@ let planets: Planets = [
         name: "Mars",
     },
 ];
+
+dotenv.config()
+const app = express()
+const port = process.env.PORT || 3000;
+
+/* middleware posizionati prima delle routes */
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'internal error 500' });
+});
+
+/* Routes */
+
+app.get('/planets', (req, res) => {
+    res.json(planets)
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`)
+})
