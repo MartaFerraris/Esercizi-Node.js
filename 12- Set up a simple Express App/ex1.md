12009989c7f559e4d8d66398c42b935a48bb0ced
Marta Ferraris <141126985+MartaFerraris@users.noreply.github.com>
2024-01-19 18:57:41 +0100

    exercise 15

--- 12- Set up a simple Express App/ex1.md
+++ 12- Set up a simple Express App/ex1.md
@@ -1,36 +1,60 @@
-# NodeJS Part 2 :: Exercise 1 :: Set up a simple Express App
+# NodeJS Part 2 :: Exercise 4 :: Add Postgres DB
 
 ## Do
 
-- Write simple Express server that listens on port `3000` (use dotenv to specify the port)
-- Create a dummy "database" of `planets` using a `let` variable. (You will use this data in further exercises.)
-- Configure your app (`app.use()`) to:
-  - accept JSON from the Client
-  - log the Client's requests
+- Using PgAdmin:
+  - Create a Postgres DB.
+- Using a `setupDb` function:
+  - Create `planets` table.
+  - Populate the table with two planets (e.g. `'Earth'` and `'Mars'`).
+- Connect your app to Postgres using Express (`pg-promise`). [https://github.com/vitaly-t/pg-promise]
+- Replace the dummy DB with the Postgres DB.
+- Rewrite all planets controller functions. They should now work with the DB. (Use the SQL queries below.)
 
 ## Use
 
-- Dummy database with initial data:
+- SQL query to create the table:
 
-  ```js
-  type Planet = {
-    id: number,
-    name: string,
-  };
+  ```sql
+  DROP TABLE IF EXISTS planets;
 
-  type Planets = Planet[];
-
-  let planets: Planets = [
-    {
-      id: 1,
-      name: "Earth",
-    },
-    {
-      id: 2,
-      name: "Mars",
-    },
-  ];
+  CREATE TABLE planets(
+    id SERIAL NOT NULL PRIMARY KEY,
+    name TEXT NOT NULL,
+  );
   ```
 
-- `express-async-errors`
-- `morgan`
+- Make sure that all CRUD operations read from and write to Postgres (instead of the dummy db you've been using in previous exercises).
+  - `GET /planets`
+    - Use this SQL query:
+      ```sql
+      SELECT * FROM planets;
+      ```
+  - `GET /planets/:id`
+    - Use this SQL query:
+      ```sql
+      SELECT * FROM planets WHERE id=$1;
+      ```
+    - Make sure that `$1` is `id`.
+  - `POST /planets`
+    - Use this SQL query:
+      ```sql
+      INSERT INTO planets (name) VALUES ($1);
+      ```
+    - Make sure that `$1` is `name`.
+  - `PUT /planets/:id`
+    - Use this SQL query:
+      ```sql
+      UPDATE planets SET name=$2 WHERE id=$1;
+      ```
+    - Make sure that `$1` is `id` and `$2` is `name`.
+  - `DELETE /planets/:id`
+    - Use this SQL query:
+      ```sql
+      DELETE FROM planets WHERE id=$1;
+      ```
+    - Make sure that `$1` is `id`.
+
+## Check
+
+- Use Postman to test the routes.
--- 12- Set up a simple Express App/server.ts
+++ 12- Set up a simple Express App/server.ts
@@ -1,16 +1,131 @@
-import * as planetsController from './controllers/planets';
 import express from 'express';
+import Joi from 'joi';
+import dotenv from 'dotenv'
+import pgPromise from 'pg-promise';
 
+const db = pgPromise()("postgres://postgres:250394@localhost:5432/esercizio")
+
+const setupDb = async () => {
+    await db.none(`
+        DROP TABLE IF EXISTS planets;
+
+        CREATE TABLE planets (
+            id SERIAL NOT NULL PRIMARY KEY,
+            name TEXT NOT NULL
+        );
+    `)
+
+    await db.none(`INSERT INTO planets (name) VALUES ('Earth')`)
+    await db.none(`INSERT INTO planets (name) VALUES ('Mars')`)
+
+    const planets = await db.many(`SELECT * FROM planets;`)
+    console.log(planets);
+}
+setupDb()
+
+type Planet = {
+    id: number,
+    name: string,
+};
+
+type Planets = Planet[];
+
+let planets: Planets = [
+    {
+        id: 1,
+        name: "Earth",
+    },
+    {
+        id: 2,
+        name: "Mars",
+    },
+];
+
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/api/planets', planetsController.getAll);
-app.get('/api/planets/:id', planetsController.getOneById);
-app.post('/api/planets', planetsController.create);
-app.put('/api/planets/:id', planetsController.updateById);
-app.delete('/api/planets/:id', planetsController.deleteById);
+/* Middleware di validazione per Joi */
+const validatePlanet = (planet: Planet) => {
+    const schema = Joi.object({
+        id: Joi.number().integer().min(1).required(),
+        name: Joi.string().required(),
+    });
+
+    return schema.validate(planet);
+};
+
+/* Routes */
+
+app.get('/api/planets', async (req, res) => {
+    const dbPlanets = await db.many(`SELECT * FROM planets;`);
+    res.json(dbPlanets);
+});
+
+app.get('/api/planets/:id', async (req, res) => {
+    const planetId = parseInt(req.params.id);
+
+    try {
+        const dbPlanet = await db.oneOrNone('SELECT * FROM planets WHERE id = $1', [planetId]);
+
+        if (!dbPlanet) {
+            return res.status(404).json({ error: 'Planet not found' });
+        }
+
+        res.json(dbPlanet);
+    } catch (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.post('/api/planets', async (req, res) => {
+    const newPlanet: Planet = req.body;
+    const { error } = validatePlanet(newPlanet);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    try {
+        await db.none('INSERT INTO planets (name) VALUES ($1)', [newPlanet.name]);
+        res.status(201).json({ msg: 'Planet created successfully' });
+    } catch (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.put('/api/planets/:id', async (req, res) => {
+    const planetId = parseInt(req.params.id);
+    const updatedPlanet: Planet = req.body;
+
+    const { error } = validatePlanet(updatedPlanet);
+    if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+    }
+
+    try {
+        await db.none('UPDATE planets SET name=$1 WHERE id=$2', [updatedPlanet.name, planetId]);
+        res.json({ msg: 'Planet updated successfully' });
+    } catch (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.delete('/api/planets/:id', async (req, res) => {
+    const planetId = parseInt(req.params.id);
+
+    try {
+        await db.none('DELETE FROM planets WHERE id=$1', [planetId]);
+        res.json({ msg: 'Planet deleted successfully' });
+    } catch (err) {
+        console.error('Error executing query:', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
